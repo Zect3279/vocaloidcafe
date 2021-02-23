187ef9828ee7293fdf3eb1e869eb6bd7dc308515
amachi16384 <68372504+amachi16384@users.noreply.github.com>
2021-02-23 13:08:44 +0900

    Update log.py

--- cogs/log.py
+++ cogs/log.py
@@ -15,21 +15,21 @@ class Log(commands.Cog):
 
         free_room = self.bot.get_channel(808307478470459412)
 
-        if before.channel is not None:
-            if before.channel.category == free_room:
-                channel = discord.utils.get(free_room.text_channels, name = before.channel.name)
-                embed = discord.Embed(description = f'{member.mention}さんが退室しました')
-                msg = await channel.send(embed = embed)
-                await asyncio.sleep(5)
-                await msg.delete()
-                
         if after.channel is not None:
             if after.channel.category == free_room:
                 channel = discord.utils.get(free_room.text_channels, name = after.channel.name)
                 embed = discord.Embed(description = f'{member.mention}さんが入室しました')
                 msg = await channel.send(embed = embed)
-                await asyncio.sleep(5)
+                await asyncio.sleep(3)
                 await msg.delete()
-  
+                
+        if before.channel is not None:
+            if before.channel.category == free_room:
+                channel = discord.utils.get(free_room.text_channels, name = before.channel.name)
+                embed = discord.Embed(description = f'{member.mention}さんが退室しました')
+                msg = await channel.send(embed = embed)
+                await asyncio.sleep(3)
+                await msg.delete()
+
 def setup(bot):
     bot.add_cog(Log(bot))
