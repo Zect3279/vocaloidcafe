912527e9cdee1885008304b471ec1a027cd04a8d
amachi16384 <68372504+amachi16384@users.noreply.github.com>
2021-03-17 19:37:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,11 @@
 import os
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=".", intents=intents)
+slash = SlashCommand(bot, sync_commands = True)
 
 cogs = [
     'cogs.system',
