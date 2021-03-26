cb0027d364afa0d558721d07ebe2ce3a70027477
amachi16384 <68372504+amachi16384@users.noreply.github.com>
2021-03-26 10:00:32 +0900

    Update highlow.py

--- cogs/minigame/highlow.py
+++ cogs/minigame/highlow.py
@@ -45,7 +45,7 @@ class Highlow(commands.Cog):
 
             if reaction.emoji == '⤴️' and me > you or reaction.emoji == '⤵️' and me < you:
                 mag = (7 - abs(7 - you)) / 7 + 1
-                msg = await ctx.send(f'```自分のカードは {int(me)} でした\n正解 {int(bet)} -> {int(bet * mag)}\n続行しますか?```')
+                msg = await ctx.send(f'```自分のカードは {me} でした\n正解 {int(bet)} -> {int(bet * mag)}\n続行しますか?```')
                 bet *= mag
 
                 await msg.add_reaction('⭕')
@@ -69,7 +69,7 @@ class Highlow(commands.Cog):
                     break
 
             else:
-                await ctx.send('自分のカードは {me} でした\n不正解 賭け金を全て失いました')
+                await ctx.send(f'自分のカードは {me} でした\n不正解 賭け金を全て失いました')
                 break
 
 def setup(bot):
